96798fbcb316b569356054b2af105561c9bab127
brogleyr <ryanbrogley@gmail.com>
2025-05-29 13:03:18 -0400

    Caddy commands run in sequence

--- digicred/crms-startup.sh
+++ digicred/crms-startup.sh
@@ -14,6 +14,22 @@ CADDYFILE_EXAMPLE="$DIGICRED_DIR/Caddyfile.example"
 # Variables to find and replace in traction .env file
 PUBLIC_DOMAIN=$(curl ifconfig.me)
 
+
+# Clone the repository if not already cloned
+if [ ! -d $PROJECT_DIR ]; then
+    echo "Cloning the Traction repository..."
+    git clone https://github.com/DigiCred-Holdings/traction-docker-compose "$PROJECT_DIR"
+fi
+
+
+# Prompt user input for variables
+read -p "Enter the public domain for your CrMS (default - this machine's public IP):" INPUT_DOMAIN
+if [ -n "$INPUT_DOMAIN" ]; then
+    PUBLIC_DOMAIN="$INPUT_DOMAIN"
+fi
+
+read -p "Enter the email you would like to use for the proxy:" INPUT_EMAIL
+
 # Update system packages
 echo "Updating system packages..."
 sudo apt update && sudo apt upgrade -y
@@ -34,26 +50,18 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --y
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update && sudo apt install caddy -y
 
+# Caddyfile setup
+echo "Configuring Caddyfile"
+sudo touch /etc/caddy/Caddyfile
+sudo cp $CADDYFILE_EXAMPLE /etc/caddy/Caddyfile
+
+sudo sed -i "s|<public-domain>|$PUBLIC_DOMAIN|g" /etc/caddy/Caddyfile
+sudo sed -i "s|<email>|$INPUT_EMAIL|g" /etc/caddy/Caddyfile
+
 # Enable and start Caddy
 echo "Enabling and starting Caddy..."
 sudo systemctl enable caddy
 sudo systemctl restart caddy
-
-# Clone the repository if not already cloned
-if [ ! -d $PROJECT_DIR ]; then
-    echo "Cloning the Traction repository..."
-    git clone https://github.com/DigiCred-Holdings/traction-docker-compose "$PROJECT_DIR"
-fi
-
-
-# Prompt user input for variables
-read -p "Enter the public domain for your CrMS (default - this machine's public IP):" INPUT_DOMAIN
-if [ -n "$INPUT_DOMAIN" ]; then
-    PUBLIC_DOMAIN="$INPUT_DOMAIN"
-fi
-
-read -p "Enter the email you would like to use for the proxy:" INPUT_EMAIL
-
 
 # .env file setup
 echo "Configuring .env file"
@@ -62,13 +70,5 @@ cp -f $ENV_EXAMPLE $ENV_FILE
 echo "Public domain in .env set to: $PUBLIC_DOMAIN"
 
 sed -i "s|<public-domain>|$PUBLIC_DOMAIN|g"  $ENV_FILE
-
-# Caddyfile setup
-echo "Configuring Caddyfile"
-sudo touch /etc/caddy/Caddyfile
-sudo cp $CADDYFILE_EXAMPLE /etc/caddy/Caddyfile
-
-sudo sed -i "s|<public-domain>|$PUBLIC_DOMAIN|g" /etc/caddy/Caddyfile
-sudo sed -i "s|<email>|$INPUT_EMAIL|g" /etc/caddy/Caddyfile
 
 echo "Setup complete!"
